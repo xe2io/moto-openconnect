bc82b0fc9e09348e135f6d63faa472531924d4ec
Jonathan Chen-DKTC47 <jcchen@motorolasolutions.com>
2018-09-19 10:27:27 -0500

    add script to handle auth with new portal

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,15 @@ RUN apt-get update && apt-get install -y \
     ca-certificates \
     curl \
     openconnect \
-    ocproxy \
     openssh-server \
     iputils-ping \
     iputils-tracepath \
     dnsutils \
+    jq \
 	--no-install-recommends \
 	&& rm -rf /var/lib/apt/lists/*
 
-COPY mototunnel vpn_uptime /
+COPY mototunnel vpn_login.sh vpn_uptime /
 
 # Remove the apt proxy
 #RUN rm /etc/apt/apt.conf.d/01proxy
---
+++ vpn_login.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+URL_PORTAL='https://access.motorolasolutions.com'
+URL_LOGIN="${URL_PORTAL}/dana-na/auth/url_default/login.cgi"
+
+# Okta Actions; 1 = Okta Verify (code; not supported); 2 = Okta Push
+okta_action='2'
+
+realm='OKTA+Login'
+
+# TODO: Useragent?
+# Script requires: jq curl awk openconnect
+DEPENDENCIES="jq curl awk openconnect"
+for d in $DEPENDENCIES; do
+    which $d > /dev/null
+    if [[ 0 -ne $? ]]; then
+        echo "This utility requires '$d'."
+        exit 1
+    fi
+done
+
+### PHASE 1: Authenticate using DS Domain credentials
+
+# Prompt for credentials
+echo -n "Username: "
+read user
+echo -n "Password: "
+stty -echo
+read pass
+stty echo
+
+url_pass=$(echo -n $pass | jq -s -R -r @uri)
+
+unset pass
+
+data="username=${user}&password=${url_pass}&realm=${realm}&btnSubmit=Sign+In"
+unset url_pass
+
+# -H 'Content-Type: application/x-www-form-urlencoded'
+
+echo -e "\n\nLogging in to Access Motorola Portal..."
+p1_url=$(curl -s ${URL_LOGIN} --data "${data}" -o /dev/null -w '%{redirect_url}')
+unset data
+
+# Check the 302 redirect URL
+# Failure: https://access.motorolasolutions.com/dana-na/auth/url_default/welcome.cgi?p=failed
+# Success: https://access.motorolasolutions.com/dana-na/auth/url_default/welcome.cgi?p=defender&id=state_####
+if [[ "$p1_url" == *failed ]]; then
+    echo "Failed login.  Please check credentials."
+    exit 1
+fi
+
+# Grab the defender state ID to pass to phase 2
+defender_id=${p1_url##*defender&id=}
+
+### PHASE 2: Choose Okta Push (2) for 2FA
+data="username=${user}&key=${defender_id}&password=${okta_action}&btnAction=++Sign+In++"
+
+# -H "Referer: $p1_url"
+
+echo "Requesting Okta Push.  Please acknowledge 2FA to continue..."
+cookie_file=$(mktemp)
+
+curl -L -s ${URL_LOGIN} -o /dev/null --data "${data}" -c $cookie_file
+unset data
+
+vpn_cookie=$(awk 'match($0, /DSID.*[a-f0-9]+/) {printf "%s=%s", $(NF-1), $NF}' $cookie_file)
+rm $cookie_file
+
+### Get VPN cookie (DSID=____) from cookie, pass to openconnect
+if [[ -z "$vpn_cookie" ]]; then
+    echo "Failed to get VPN cookie.  Please try again."
+    exit 1
+fi
+
+echo "VPN Cookie: $vpn_cookie"
+echo -e "Launching OpenConnect with VPN Cookie..."
+openconnect --juniper --timestamp -C "$vpn_cookie" $URL_PORTAL
